f5c2a81698df598fc21252ce33908f388b798bc4
gord0186 <gord0186@algonqiuinlive.com>
2014-10-07 10:27:10 -0400

    shuttle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var $win = $(window);
 var $sun = $('.sun')
 var $sunsection = $('.sun-section');
+var $dippersection = $('.dipper-section');
+var $dipper = $('.dipper');
+var $shuttlesection = $('.shuttle-section');
+var $shuttle = $('.shuttle');
+var $shuttlesectionmove = $('.shuttle-section');
+var $shuttlemove = $('.shuttle');
 
 $win.on('scroll', function(){
 	var scrollpos = $win.scrollTop();
@@ -8,3 +14,12 @@ $win.on('scroll', function(){
 	$sun.css('transform', 'rotate('+ scrollpos / 5 + 'deg)');	
 	$sunsection.css('background-position', 'center ' + scrollpos/3 + 'px')
 })
+
+$dippersection.waypoint(function (){
+	$dipper.addClass('js-dipper-fade')
+},{ offset: '50%' });
+
+$shuttlesection.waypoint(function (){
+	$shuttle.addClass('js-shuttle-fade')
+},{ offset: '50%' });
+
